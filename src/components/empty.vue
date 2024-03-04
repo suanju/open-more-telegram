6f502a0f1047e32dc72212e5efa7d034934e0191
橡皮擦 <2506152074@qq.com>
2024-03-04 08:28:47 +0800

    空页组件

---
+++ src/components/empty.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="w-full h-screen" style="background-color: #212121;">
+        <div>
+
+        </div>
+        <div class="h-screen flex flex-col justify-center items-center text-gray-100 font-bold">
+            <div>
+                <AddUserIcon class="W-32 h-32" />
+            </div>
+            <span class="mt-8">
+                <button class="border border-blue-500 text-blue-500 w-28 font-bold py-2 px-4 rounded-3xl
+                hover:bg-blue-500 hover:text-white hover:border-blue-500" @click="userStore.addUser">
+                    添加
+                </button>
+            </span>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { useUserStore } from "@/store/user"
+import AddUserIcon from "@/assets/add_user.svg"
+
+const userStore = useUserStore()
+</script>
